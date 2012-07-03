b9856ea49b495ba9371b0b7ff2c8b1ecd46c6b50
Tomaz Cerar <tomaz.cerar@gmail.com>
2012-07-03 12:49:47 +0200

    layout & pom.xml cleanup

--- src/main/java/org/openblend/fejstbuk/ui/RestfullWallImpl.java
+++ src/main/java/org/openblend/fejstbuk/ui/RestfullWallImpl.java
@@ -1,5 +1,14 @@
 package org.openblend.fejstbuk.ui;
 
+import org.openblend.fejstbuk.domain.Owned;
+import org.openblend.fejstbuk.domain.User;
+import org.openblend.fejstbuk.qualifiers.Current;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,16 +16,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
-import org.openblend.fejstbuk.domain.Owned;
-import org.openblend.fejstbuk.domain.User;
-import org.openblend.fejstbuk.qualifiers.Current;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
@@ -35,12 +34,12 @@ public class RestfullWallImpl implements RestfullWall {
         Set<User> users = new HashSet<User>(current.getFriends());
         users.add(current);
         // user and his friend's posts
-        Query query = em.createQuery("select from Post p where p.user in (:users) order by p.timestamp desc");
+        Query query = em.createQuery("select p from Post p where p.user in (:users) order by p.timestamp desc");
         query.setMaxResults(Wall.SIZE);
         query.setParameter("users", users);
         list.addAll(query.getResultList());
         // user and his friend's likes
-        query = em.createQuery("select from Like l where l.user in (:users) order by l.timestamp desc");
+        query = em.createQuery("select l from Like l where l.user in (:users) order by l.timestamp desc");
         query.setMaxResults(Wall.SIZE);
         query.setParameter("users", users);
         list.addAll(query.getResultList());
@@ -62,7 +61,7 @@ public class RestfullWallImpl implements RestfullWall {
 
     private static class OwnedComparator implements Comparator<Owned> {
         public int compare(Owned o1, Owned o2) {
-            return (int)(o2.getTimestamp().getTime() - o1.getTimestamp().getTime());
+            return (int) (o2.getTimestamp().getTime() - o1.getTimestamp().getTime());
         }
     }
 }
