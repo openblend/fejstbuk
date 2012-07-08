3acf603ad4553bbeb685d9008f2d1df4c654123d
Tomaz Cerar <tomaz.cerar@gmail.com>
2012-07-08 23:12:37 +0200

    fix for hibernate strange bug

--- src/main/java/org/openblend/fejstbuk/domain/Like.java
+++ src/main/java/org/openblend/fejstbuk/domain/Like.java
@@ -2,11 +2,13 @@ package org.openblend.fejstbuk.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 @Entity
+@Table(name = "LIKES")
 public class Like extends Owned {
     private Post post;
 
