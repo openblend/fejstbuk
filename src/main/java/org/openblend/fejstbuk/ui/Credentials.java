792597d66226f18175fd8b1a45fa291f9806b99f
Marko Strukelj <marko.strukelj@gmail.com>
2012-07-08 23:53:20 +0200

    Login

--- src/main/java/org/openblend/fejstbuk/ui/Credentials.java
+++ src/main/java/org/openblend/fejstbuk/ui/Credentials.java
@@ -1,12 +1,14 @@
 package org.openblend.fejstbuk.ui;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Model;
+import javax.inject.Named;
 
 /**
  * @author <a href="mailto:marko.strukelj@gmail.com">Marko Strukelj</a>
  */
-
-@Model
+@RequestScoped
+@Named("credentials")
 public class Credentials
 {
    private String username;
