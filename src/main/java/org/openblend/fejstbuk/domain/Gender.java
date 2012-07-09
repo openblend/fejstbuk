f8cdc0d2c057d09e16a2b26f30970e6d4d955a99
Matej Lazar <matejonnet@gmail.com>
2012-07-09 22:32:51 +0200

    Profile edit.

--- src/main/java/org/openblend/fejstbuk/domain/Gender.java
+++ src/main/java/org/openblend/fejstbuk/domain/Gender.java
@@ -4,5 +4,16 @@ package org.openblend.fejstbuk.domain;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public enum Gender {
-    MALE, FEMALE
+    MALE("Male"),
+    FEMALE("Female");
+
+    private final String label;
+
+    private Gender(String label) {
+      this.label = label;
+    }
+
+    public String getLabel() {
+      return this.label;
+    }
 }
