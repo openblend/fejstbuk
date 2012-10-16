ccbf4bdd74a5c8578be11063ac0960a46eb953d3
Marko Strukelj <marko.strukelj@gmail.com>
2012-10-16 22:38:23 +0200

    Convert User.java into an @Entity, and enable JPA

--- src/main/java/org/openblend/fejstbuk/model/User.java
+++ src/main/java/org/openblend/fejstbuk/model/User.java
@@ -1,11 +1,20 @@
 package org.openblend.fejstbuk.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author <a href="mailto:marko.strukelj@gmail.com">Marko Strukelj</a>
  */
+@Entity
 public class User
 {
+   @Id
    private String username;
+
+   private String password;
+
+   public User() {}
 
    public User(String username) {
       this.username = username;
@@ -13,5 +22,19 @@ public class User
 
    public String getUsername() {
       return username;
+   }
+
+   public void setUsername(String username) {
+
+   }
+
+   public String getPassword()
+   {
+      return password;
+   }
+
+   public void setPassword(String password)
+   {
+      this.password = password;
    }
 }
